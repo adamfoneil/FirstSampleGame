77f88eda79b1d8315fe1d7b5837c0c44742a3824
Adam O'Neil <adamosoftware@gmail.com>
2024-04-09 21:52:06 -0400

    minor cleanup

--- GameCore.cs
+++ GameCore.cs
@@ -3,47 +3,45 @@ using Chroma.ContentManagement;
 using Chroma.ContentManagement.FileSystem;
 using Chroma.Diagnostics.Logging;
 using Chroma.Graphics;
-using Chroma.Input;
 using FirstSampleGame.Abstract;
 using FirstSampleGame.Entities;
 using System;
 using System.Collections.Generic;
 using System.IO;
 
-namespace FirstSampleGame
+namespace FirstSampleGame;
+
+internal class GameCore : Game
 {
-    internal class GameCore : Game
+    private Log Log { get; } = LogManager.GetForCurrentAssembly();
+
+    private IEnumerable<Entity> Entities =
+    [
+        new Player(new(300), new(200)),
+    ];
+
+    internal GameCore()
     {
-        private Log Log { get; } = LogManager.GetForCurrentAssembly();
+    }
 
-        private IEnumerable<Entity> Entities =
-        [
-            new Player(new(300), new(200)),
-        ];
+    protected override void LoadContent()
+    {
+        foreach (var e in Entities) e.LoadTexture(Content);
+    }
 
-        internal GameCore()
-        {
-        }
+    protected override IContentProvider InitializeContentPipeline() =>
+        new FileSystemContentProvider(Path.Combine(AppContext.BaseDirectory, "Resources"));       
 
-        protected override void LoadContent()
-        {
-            foreach (var e in Entities) e.LoadTexture(Content);
-        }
+    protected override void Draw(RenderContext context)
+    {
+        foreach (var e in Entities) context.DrawTexture(e.Texture, e.Position, new(1));
+    }
 
-        protected override IContentProvider InitializeContentPipeline() =>
-            new FileSystemContentProvider(Path.Combine(AppContext.BaseDirectory, "Resources"));       
+    protected override void Update(float delta)
+    {
+        foreach (var e in Entities) e.Update(delta);
+        
 
-        protected override void Draw(RenderContext context)
-        {
-            foreach (var e in Entities) context.DrawTexture(e.Texture, e.Position, new(1));
-        }
-
-        protected override void Update(float delta)
-        {
-            foreach (var e in Entities) e.Update(delta);
-            
-
-            base.Update(delta);
-        }
+        base.Update(delta);
     }
 }
