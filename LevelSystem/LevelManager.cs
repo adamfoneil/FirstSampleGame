bece4ca0aa7b41c053c069c2edac9349c8b0fa54
scott slaby <100446621+scottdev333@users.noreply.github.com>
2024-04-28 03:02:34 -0500

    Load Paths from json file

--- LevelSystem/LevelManager.cs
+++ LevelSystem/LevelManager.cs
@@ -1,17 +1,28 @@
 using System.Collections.Generic;
 using System.Numerics;
+using FirstSampleGame;
+using System;
 
 public class LevelManager
 {
 
 	private List<LevelRecord> Levels;
 	private int CurrentLevel;
+    private dynamic navPaths;
 
 	public LevelManager()
 	{
 		Levels = new List<LevelRecord>();
 		CurrentLevel = 1;
+        LoadShapePaths();
 	}
+
+    public void LoadShapePaths()
+    {
+        dynamic jsonObject = GameCore.LoadJsonFile("paths.json");
+        navPaths = jsonObject.paths;
+        //Console.WriteLine($"navPaths {navPaths[0]}");
+    }
 
 	public void CreateLevel(string textureName, Vector2 playerSpawnPosition, List<SpawnRecord> spawnRecords)
 	{
@@ -30,10 +41,23 @@ public class LevelManager
 	{
 		List<SpawnRecord> spawnRecords =
 		[
-			new SpawnRecord(0, 150f , 1f, 100, PathNav.GetShapePath(2), new Vector2(700f, 540f)),
-			new SpawnRecord(1, 150f , 1f, 100, PathNav.GetShapePath(3), new Vector2(700f, 540f)),
+			new SpawnRecord(0, 150f , 1f, 100, GetShapePath(2), new Vector2(700f, 540f)),
+			new SpawnRecord(1, 150f , 1f, 100, GetShapePath(3), new Vector2(700f, 540f)),
 		];
 		CreateLevel("test-level.png", new Vector2(200, 300), spawnRecords);
 	}
+
+    public dynamic GetShapePath(int pathIndex)
+    {
+        dynamic path = navPaths[pathIndex];
+        List<Vector2> fPath = new List<Vector2>();
+        foreach (var waypoint in path)
+        {
+            Vector2 floatWaypoint = new Vector2((float)waypoint.X, (float)waypoint.Y);
+            fPath.Add(floatWaypoint);
+            Console.WriteLine($"path: {floatWaypoint}");
+        }
+        return fPath;
+    }
 
 }
